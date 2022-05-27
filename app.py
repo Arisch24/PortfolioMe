ec9907c9a217ebf0149d40c7e88bb5bf80501fa5
Arisch24 <arischvaran00@gmail.com>
2022-05-27 21:05:46 +0800

    Basic route and forms done

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request, flash
+from forms import EditProfileForm, RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "383ed93c7f5c33627fb4bf56036c7493"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
 
 @app.route("/")
+@app.route("/index")
 def home():
-    return "Hello, Flask haah"
+    return render_template("index.html")
 
-if __name__ == "__name__":
-    app.run()
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if(request.method == "POST"):
+        if form.validate_on_submit():
+            flash(f"Account created for {form.username.data}!", "success")
+        else:
+            flash(f"Please fix the errors first.", "failed")
+
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if(request.method == "POST"):
+        if form.validate_on_submit():
+            if form.email.data == 'admin@admin.com' and form.password.data == 'password':
+                flash(f"Successful login!", "success")
+                return redirect(url_for("home"))
+            else:
+                flash(f"Login Unsuccessful!", 'failed')
+    return render_template("login.html", form=form)
+
+@app.route("/edit-profile", methods=["GET", "POST"])
+def edit_profile():
+    form = EditProfileForm()
+    if(request.method == "POST"):
+        if form.validate_on_submit():
+            flash(f"Profile updated successfully.", "success")
+        else:
+            flash(f"There were some errors.", "failure")
+    return render_template("edit_profile.html", form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,6 +1,3 @@
-from ast import Str, Sub
-from sre_parse import FLAGS
-from tokenize import String
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
@@ -23,3 +20,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=8, max=11)])
+    # Radio field
+    radio = ['Male', 'Female']
+    gender = RadioField('Gender', validators=[DataRequired()], choices=radio)
+    # 
+    organization = StringField('Organization', validators=[DataRequired()])
+    submit = SubmitField('Save Changes')
