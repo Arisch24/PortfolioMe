9327c0b7ceb7a9090dbf0ac78e6d7ae4b8ca768f
Arisch24 <arischvaran00@gmail.com>
2022-05-28 18:09:57 +0800

    Simple formatting

--- forms.py
+++ forms.py
@@ -2,18 +2,23 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=8, max=11)])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password')])
+    phone_number = StringField('Phone Number', validators=[
+        DataRequired(), Length(min=8, max=11)])
     # Radio field
-    radio = ['Male', 'Female']
+    radio = ["Male", "Female", "Prefer not to say"]
     gender = RadioField('Gender', validators=[DataRequired()], choices=radio)
-    # 
+    #
     organization = StringField('Organization', validators=[DataRequired()])
     submit = SubmitField('Register')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -21,14 +26,17 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('Username', validators=[
+                        DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    phone_number = StringField('Phone Number', validators=[DataRequired(), Length(min=8, max=11)])
+    phone_number = StringField('Phone Number', validators=[
+                        DataRequired(), Length(min=8, max=11)])
     # Radio field
     radio = ['Male', 'Female']
     gender = RadioField('Gender', validators=[DataRequired()], choices=radio)
-    # 
+    #
     organization = StringField('Organization', validators=[DataRequired()])
     submit = SubmitField('Save Changes')
