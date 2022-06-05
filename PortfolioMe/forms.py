76d6b4db933d7688617852ffa8016dc8e9d8200e
Arisch24 <arischvaran00@gmail.com>
2022-06-05 19:09:58 +0800

    Added more validations

--- PortfolioMe/forms.py
+++ PortfolioMe/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField, FileField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
         DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message="Minimum length is %(min)d characters.")])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password')])
     phone_number = StringField('Phone Number', validators=[
-        DataRequired(), Length(min=8, max=11)])
+        DataRequired(), Length(min=8, max=11, message="Follows Malaysian phone number format."), Regexp(r"(01)[0-9]{1}[0-9]{6,8}", message="Phone number is 9 to 11 characters without dash.")])
     # Radio field
     radio = ["Male", "Female", "Prefer not to say"]
     gender = RadioField('Gender', validators=[DataRequired()], choices=radio)
@@ -31,9 +31,9 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[
                         DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message="Minimum length is %(min)d characters.")])
     phone_number = StringField('Phone Number', validators=[
-                        DataRequired(), Length(min=8, max=11)])
+                        DataRequired(), Length(min=8, max=11, message="Follows Malaysian phone number format."), Regexp(r"(01)[0-9]{1}[0-9]{6,8}", message="Phone number is 9 to 11 characters without dash.")])
     # Radio field
     radio = ["Male", "Female", "Prefer not to say"]
     gender = RadioField('Gender', validators=[DataRequired()], choices=radio)
