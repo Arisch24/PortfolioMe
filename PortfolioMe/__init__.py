5a249187ecfd375535f88ca4d2de37c7078bd21c
Arisch24 <arischvaran00@gmail.com>
2022-06-07 21:23:34 +0800

    Bcrypt and login manager added to app

--- PortfolioMe/__init__.py
+++ PortfolioMe/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "383ed93c7f5c33627fb4bf56036c7493"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from PortfolioMe import routes
