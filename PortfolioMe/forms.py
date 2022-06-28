5c677ac64c91bc3043bebbca0bb6413e844d21e5
Arisch24 <arischvaran00@gmail.com>
2022-06-28 12:56:27 +0800

    Password resetting forms created

--- PortfolioMe/forms.py
+++ PortfolioMe/forms.py
@@ -5,11 +5,13 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, Radio
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, ValidationError
 from PortfolioMe.models import Applicant
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
         DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message="Minimum length is %(min)d characters.")])
+    password = PasswordField('Password', validators=[DataRequired(), Length(
+        min=8, message="Minimum length is %(min)d characters.")])
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(), EqualTo('password')])
     phone_number = TelField('Phone Number', validators=[
@@ -24,12 +26,14 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         applicant = Applicant.query.filter_by(username=username.data).first()
         if applicant:
-            raise ValidationError('This username is taken. Please choose a different one.')
+            raise ValidationError(
+                'This username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         applicant = Applicant.query.filter_by(email=email.data).first()
         if applicant:
-            raise ValidationError('This email is taken. Please choose a different one.')
+            raise ValidationError(
+                'This email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -41,10 +45,10 @@ class LoginForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[
-                        DataRequired(), Length(min=3, max=20)])
+        DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone_number = TelField('Phone Number', validators=[
-                        DataRequired(), Length(min=8, max=11, message="Follows Malaysian phone number format."), Regexp(r"(01)[0-9]{1}[0-9]{6,8}", message="Phone number is 9 to 11 characters without dash.")])
+        DataRequired(), Length(min=8, max=11, message="Follows Malaysian phone number format."), Regexp(r"(01)[0-9]{1}[0-9]{6,8}", message="Phone number is 9 to 11 characters without dash.")])
     # Radio field
     radio = ["Male", "Female", "Prefer not to say"]
     gender = RadioField('Gender', validators=[DataRequired()], choices=radio)
@@ -54,24 +58,49 @@ class EditProfileForm(FlaskForm):
 
     def validate_username(self, username):
         if username.data != current_user.username:
-            applicant = Applicant.query.filter_by(username=username.data).first()
+            applicant = Applicant.query.filter_by(
+                username=username.data).first()
             if applicant:
-                raise ValidationError('This username is taken. Please choose a different one.')
+                raise ValidationError(
+                    'This username is taken. Please choose a different one.')
 
     def validate_email(self, email):
         if email.data != current_user.email:
             applicant = Applicant.query.filter_by(email=email.data).first()
             if applicant:
-                raise ValidationError('This email is taken. Please choose a different one.')
+                raise ValidationError(
+                    'This email is taken. Please choose a different one.')
 
 
 class ResumeSubmissionForm(FlaskForm):
-    resume = FileField('Resume', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
+    resume = FileField('Resume', validators=[
+                       FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'])])
     output = TextAreaField('Output', validators=None)
     submit = SubmitField('Send resume')
 
 
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        applicant = Applicant.query.filter_by(email=email.data).first()
+        if not applicant:
+            raise ValidationError(
+                'There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired(), Length(
+        min=8, message="Minimum length is %(min)d characters.")])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
 '''Admin Forms'''
+
+
 class AdminLoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
