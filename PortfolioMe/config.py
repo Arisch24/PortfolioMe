afb12aa98281c6cc335dee27832b30a034584e76
Arisch24 <arischvaran00@gmail.com>
2022-07-03 01:39:23 +0800

    Disabled track modifications sqlalchemy

--- PortfolioMe/config.py
+++ PortfolioMe/config.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     FLASK_ADMIN_SWATCH = "litera"
