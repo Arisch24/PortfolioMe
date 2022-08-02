5d5831d6f40c8b2b21ebdd602fd0749c817169e1
Arisch24 <arischvaran00@gmail.com>
2022-08-02 23:47:16 +0800

    Finally flask-apscheduler is working
    Gets users accounts which have not been logged in for more than 3 months and sets the account status to inactive

--- PortfolioMe/__init__.py
+++ PortfolioMe/__init__.py
@@ -1,3 +1,7 @@
+import atexit
+from datetime import datetime
+from email.mime import application
+from dateutil.relativedelta import relativedelta
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -5,6 +9,7 @@ from flask_login import LoginManager
 from flask_session import Session
 from flask_mail import Mail
 from flask_admin import Admin
+from flask_apscheduler import APScheduler
 from PortfolioMe.config import Config
 
 
@@ -16,30 +21,48 @@ login_manager.login_message_category = "info"
 session = Session()
 mail = Mail()
 admin = Admin()
+scheduler = APScheduler()
 
-
-from PortfolioMe import admin_routes  # noqa
+from PortfolioMe import admin_routes, models  # noqa
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    session.init_app(app)
-    mail.init_app(app)
-    admin.init_app(app)
+    with app.app_context():
+        db.init_app(app)
+        bcrypt.init_app(app)
+        login_manager.init_app(app)
+        session.init_app(app)
+        mail.init_app(app)
+        admin.init_app(app)
+        scheduler.init_app(app)
+        scheduler.start()
 
-    from PortfolioMe.client.routes import client  # noqa
-    from PortfolioMe.auth.routes import auth  # noqa
-    from PortfolioMe.main.routes import main  # noqa
-    from PortfolioMe.errors.handlers import errors  # noqa
+        from PortfolioMe.client.routes import client  # noqa
+        from PortfolioMe.auth.routes import auth  # noqa
+        from PortfolioMe.main.routes import main  # noqa
+        from PortfolioMe.errors.handlers import errors  # noqa
 
-    app.register_blueprint(client)
-    app.register_blueprint(auth)
-    app.register_blueprint(main)
-    app.register_blueprint(errors)
+        app.register_blueprint(client)
+        app.register_blueprint(auth)
+        app.register_blueprint(main)
+        app.register_blueprint(errors)
+
+    def check_inactive_users():
+        with app.app_context():
+            today_date = datetime.utcnow() - relativedelta(months=+3)
+            applicants = models.Applicant.query.filter(
+                models.Applicant.last_active < today_date).all()
+            if applicants:
+                for applicant in applicants:
+                    applicant.status = "Inactive"
+                db.session.commit()
+
+    job = scheduler.add_job(id='1234', func=check_inactive_users,
+                            trigger='cron', minute="0", hour="2", day="*", month="*")
+
+    atexit.register(lambda: scheduler.shutdown(wait=True))
 
     return app
