6cdca820883bf6ff50bc9dd52789810a74d5e701
Arisch24 <arischvaran00@gmail.com>
2022-08-07 12:05:22 +0800

    Config file changed to use .env file

--- PortfolioMe/config.py
+++ PortfolioMe/config.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+dotenv_path = join(dirname(__file__), '.env')
+
+load_dotenv(dotenv_path=dotenv_path, override=True)
 
 
 class Config:
